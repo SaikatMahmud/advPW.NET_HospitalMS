ffe392b3c23ae982fff2e36ba59972bd8f4aa9da
Saikat Mahmud <saikat8486@gmail.com>
2023-03-16 19:45:57 +0600

    com,leave,user,ipdbill

---
+++ HospitalMS_MVC/HospitalMS_MVC/EF/Models/Complaint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalMS_MVC.EF.Models
+{
+    public class Complaint
+    {
+        public int Id { get; set; }
+
+    }
+}
