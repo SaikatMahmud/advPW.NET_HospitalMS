c54fab43db76b0c6ee769b0b55bf44d732cf7e99
Saikat Mahmud <saikat8486@gmail.com>
2023-04-10 04:00:09 +0600

    D Inversion

---
+++ HospitalMS_API/BLL/DTOs/PatientDTO.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class PatientDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        public string BloodGroup { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Type { get; set; }
+        public List<Prescription> Prescriptions { get; set; }
+        public List<Appointment> Appointments { get; set; }
+        public List<IPDAdmit> IPDAdmits { get; set; }
+        public List<PerformDiag> PerformDiags { get; set; }
+        public List<OPDBill> OPDBills { get; set; }
+        public List<Complaint> Complaints { get; set; }
+    }
+}
---
+++ HospitalMS_API/DAL/Repos/PatientRepoV_0.cs
@@ -0,0 +1,43 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class PatientRepoV_0
+    {
+        public static HMSDb db;
+        static PatientRepoV_0()
+        {
+            db = new HMSDb();
+        }
+        public static List<Patient> Get()
+        {
+            return db.Patients.ToList();
+        }
+        public static Patient Get(int id)
+        {
+            return db.Patients.Find(id);
+        }
+        public static bool Create(Patient patient)
+        {
+            db.Patients.Add(patient);
+            return db.SaveChanges() > 0;
+        }
+        public static bool Update(Patient patient)
+        {
+            var exPatient = db.Patients.Find(patient.Id);
+            db.Entry(exPatient).CurrentValues.SetValues(patient);
+            return db.SaveChanges()>0;
+        }
+        public static bool Delete(int id)
+        {
+            var exP = Get(id);
+            db.Patients.Remove(exP);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
